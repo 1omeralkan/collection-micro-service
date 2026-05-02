714dc9eeaf2985d695b624f7fc622d891eef9ef9
Ömer Alkan <120298619+1omeralkan@users.noreply.github.com>
2026-05-02 18:06:48 +0300

    collection-micro-service projesi oluşturuldu

---
+++ src/test/java/com/omeralkan/collectionmicroservice/CollectionMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omeralkan.collectionmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectionMicroServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
